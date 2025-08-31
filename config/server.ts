f3760ee81f1d8a584a651d3b883619817963f800
burakesen <eeburakesen@gmail.com>
2025-08-31 22:01:18 +0300

    chore: cloud-ready db/server config + pg

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,7 @@
 export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  url: env("PUBLIC_URL"), // Strapi Cloud burayı otomatik set eder
+  proxy: true,
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS"),
   },
 });
